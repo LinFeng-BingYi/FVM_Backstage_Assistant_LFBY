5e0dd8842c5c8e4c38deb9347b46fe67f8934625
LinFeng-BingYi <linfeng_bingyi@qq.com>
2023-11-26 23:07:09 +0800

    初始化项目目录结构

---
+++ src/AppImplement/Business/Foundation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : Linfeng_Bingyi
+# @Contact : linfengbingyi@qq.com
+# @File    : Foundation.py
+# @Time    : 2023/11/26 22:03
+# @Dsc     : 基础操作
+
---
+++ src/AppImplement/StasticValue/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  : Linfeng_Bingyi
+# @Contact : linfengbingyi@qq.com
+# @File    : __init__.py
+# @Time    : 2023/11/26 22:10
+# @Dsc     : 游戏中的固定值
+
+
+# 地图相关 -------------------------------------------------------------------------
+# 世界地图坐标
+WORLD_MAP_POS = (860, 50)
+# 世界地图中 美味岛 坐标
+MAP_MW_POS = (280, 270)
+# 世界地图中 火山岛 坐标
+MAP_HS_POS = (680, 460)
+# 世界地图中 浮空岛 坐标
+MAP_FK_POS = (700, 200)
+# 世界地图中 遗迹 坐标
+MAP_YJ_POS = (750, 460)
+# 世界地图中 海底 坐标
+MAP_HD_POS = (700, 270)
+# 世界地图中 魔塔 坐标
+MAP_MT_POS = (318, 452)
+
